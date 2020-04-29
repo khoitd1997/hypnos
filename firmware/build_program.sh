3ea81392ef2fc69fa33015e9135fdc223923f84b
khoitd1997 <khoidinhtrinh@gmail.com>
2020-04-29 16:53:50 -0700

    made toolchain board indepdendent

--- firmware/build_program.sh
+++ firmware/build_program.sh
@@ -11,7 +11,7 @@ mkdir -p build/$1
 cd build/$1 
 
 # do build
-cmake ${curr_dir} -DBOARD_NAME=$1 -DCMAKE_TOOLCHAIN_FILE=${curr_dir}/nrf_sdk/toolchain_$1.cmake && cmake --build .
+cmake ${curr_dir} -DBOARD_NAME=$1 -DCMAKE_TOOLCHAIN_FILE=${curr_dir}/nrf_sdk/toolchain.cmake && cmake --build .
 
 # flash the firmware
 arm-none-eabi-objcopy -O ihex hypnos_$1.elf hypnos_$1.hex
