5537b0fee85ba4d3932527d4a20dba751e22f3ee
khoitd1997 <khoidinhtrinh@gmail.com>
2020-05-10 14:35:26 -0700

    moved twi to its own module

--- firmware/src/firmware_main.cpp
+++ firmware/src/firmware_main.cpp
@@ -39,6 +39,7 @@
  */
 
 #include <stdint.h>
+
 #include "app_error.h"
 #include "ble.h"
 
@@ -54,9 +55,7 @@
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 
-#include "boards.h"
 #include "nrf_delay.h"
-#include "nrf_drv_twi.h"
 
 #include "SEGGER_SYSVIEW.h"
 
@@ -84,89 +83,9 @@
 
 #include "power_module.hpp"
 
+#include "twi_module.hpp"
+
 #include "misc_module.hpp"
-
-#define TWI_INSTANCE_ID 0
-
-#define ARDUINO_SCL_PIN 27  // SCL signal pin
-#define ARDUINO_SDA_PIN 26  // SDA signal pin
-
-/* Common addresses definition for temperature sensor. */
-#define LM75B_ADDR (0x90U >> 1)
-
-#define LM75B_REG_TEMP 0x00U
-#define LM75B_REG_CONF 0x01U
-#define LM75B_REG_THYST 0x02U
-#define LM75B_REG_TOS 0x03U
-
-#define NORMAL_MODE 0U
-
-static volatile bool m_xfer_done = false;
-
-static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
-
-static uint8_t m_sample;
-
-void LM75B_set_mode(void) {
-  ret_code_t err_code;
-
-  /* Writing to LM75B_REG_CONF "0" set temperature sensor in NORMAL mode. */
-  uint8_t reg[2] = {LM75B_REG_CONF, NORMAL_MODE};
-  err_code       = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, sizeof(reg), false);
-  APP_ERROR_CHECK(err_code);
-  while (m_xfer_done == false)
-    ;
-
-  /* Writing to pointer byte. */
-  reg[0]      = LM75B_REG_TEMP;
-  m_xfer_done = false;
-  err_code    = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, 1, false);
-  APP_ERROR_CHECK(err_code);
-  while (m_xfer_done == false)
-    ;
-}
-
-__STATIC_INLINE void data_handler(uint8_t temp) {
-  NRF_LOG_INFO("Temperature: %d Celsius degrees.", temp);
-}
-
-/**
- * @brief TWI events handler.
- */
-void twi_handler(nrf_drv_twi_evt_t const* p_event, void* p_context) {
-  switch (p_event->type) {
-    case NRF_DRV_TWI_EVT_DONE:
-      if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX) { data_handler(m_sample); }
-      m_xfer_done = true;
-      break;
-    default:
-      break;
-  }
-}
-
-void twi_init(void) {
-  ret_code_t err_code;
-
-  const nrf_drv_twi_config_t twi_lm75b_config = {.scl                = ARDUINO_SCL_PIN,
-                                                 .sda                = ARDUINO_SDA_PIN,
-                                                 .frequency          = NRF_DRV_TWI_FREQ_100K,
-                                                 .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
-                                                 .clear_bus_init     = false};
-
-  err_code = nrf_drv_twi_init(&m_twi, &twi_lm75b_config, twi_handler, NULL);
-  APP_ERROR_CHECK(err_code);
-
-  nrf_drv_twi_enable(&m_twi);
-}
-
-static void read_sensor_data() {
-  m_xfer_done = false;
-
-  /* Read 1 byte from the specified address - skip 3 bits dedicated for fractional part of
-   * temperature. */
-  ret_code_t err_code = nrf_drv_twi_rx(&m_twi, LM75B_ADDR, &m_sample, sizeof(m_sample));
-  APP_ERROR_CHECK(err_code);
-}
 
 /**@brief Callback function for asserts in the SoftDevice.
  *
@@ -204,6 +123,7 @@ int main(void) {
   misc::timer::init();
   misc::bsp::init();
   power::init();
+  twi::init();
 
   ble::init();
   adc::init();
@@ -233,21 +153,8 @@ int main(void) {
 
   //   ble::advertising::start();
 
-  //   for (;;) {
-  //     if (!NRF_LOG_PROCESS()) { power::run(); }
-  //   }
-
-  NRF_LOG_INFO("\r\nTWI sensor example started.");
-  twi_init();
-  LM75B_set_mode();
-
-  while (true) {
-    nrf_delay_ms(500);
-
-    do { __WFE(); } while (m_xfer_done == false);
-
-    read_sensor_data();
-    NRF_LOG_FLUSH();
+  for (;;) {
+    if (!NRF_LOG_PROCESS()) { power::run(); }
   }
 }
 
---
+++ firmware/src/twi_module.cpp
@@ -0,0 +1,69 @@
+#include "twi_module.hpp"
+
+#include "ble.h"
+
+#include "nrf_drv_twi.h"
+
+#include "app_error.h"
+
+namespace twi {
+  namespace {
+    // volatile auto m_xfer_done = false;
+
+    constexpr uint32_t TWI_SCL_PIN = 27;
+    constexpr uint32_t TWI_SDA_PIN = 26;
+
+    const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(0);
+
+    // #define LM75B_ADDR (0x90U >> 1)
+
+    void twi_handler([[maybe_unused]] nrf_drv_twi_evt_t const* p_event,
+                     [[maybe_unused]] void*                    p_context) {
+      //   switch (p_event->type) {
+      //     case NRF_DRV_TWI_EVT_DONE:
+      //       if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX) { data_handler(m_sample); }
+      //       m_xfer_done = true;
+      //       break;
+      //     default:
+      //       break;
+      //   }
+    }
+
+    void wait_till_bus_free() {
+      while (nrf_drv_twi_is_busy(&m_twi)) {
+        // TODO(khoi): Check if we can run power management here
+      }
+    }
+  }  // namespace
+
+  void init() {
+    constexpr nrf_drv_twi_config_t twi_config{.scl                = TWI_SCL_PIN,
+                                              .sda                = TWI_SDA_PIN,
+                                              .frequency          = NRF_DRV_TWI_FREQ_100K,
+                                              .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+                                              .clear_bus_init     = false};
+
+    const auto err_code = nrf_drv_twi_init(&m_twi, &twi_config, twi_handler, nullptr);
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_twi_enable(&m_twi);
+  }
+
+  void read(const uint8_t address, uint8_t* p_data, const uint8_t length) {
+    // m_xfer_done = false;
+    wait_till_bus_free();
+
+    const auto err_code = nrf_drv_twi_rx(&m_twi, address, p_data, length);
+    APP_ERROR_CHECK(err_code);
+
+    wait_till_bus_free();
+  }
+  void write(const uint8_t  address,
+             uint8_t const* p_data,
+             const uint8_t  length,
+             const bool     no_stop) {
+    const auto err_code = nrf_drv_twi_tx(&m_twi, address, p_data, length, no_stop);
+    APP_ERROR_CHECK(err_code);
+    wait_till_bus_free();
+  }
+}  // namespace twi
