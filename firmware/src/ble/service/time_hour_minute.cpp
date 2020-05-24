e5cdb4f6d77004826f881058d5113b2c699df3fc
khoitd1997 <khoidinhtrinh@gmail.com>
2020-05-24 16:20:30 -0700

    added isInBreak and sleep

--- firmware/src/ble/service/time_hour_minute.cpp
+++ firmware/src/ble/service/time_hour_minute.cpp
@@ -6,9 +6,11 @@ void TimeHourMinute::set(const uint8_t hour, const uint16_t minute) {
   _data = (hour << 6) | minute;
 }
 void TimeHourMinute::get(uint8_t& hour, uint16_t& minute) const {
-  hour   = (_data >> 6) & 0b11111;
-  minute = _data & 0b111111;
+  hour   = getHour();
+  minute = getMinute();
 }
+uint8_t  TimeHourMinute::getHour() const { return (_data >> 6) & 0b11111; }
+uint16_t TimeHourMinute::getMinute() const { return _data & 0b111111; }
 
 uint8_t* TimeHourMinute::data() const { return (uint8_t*)(&_data); }
 
@@ -25,4 +27,9 @@ bool TimeHourMinute::replace(const uint8_t* buf, const size_t len) {
 
 bool operator==(const TimeHourMinute& lhs, const TimeHourMinute& rhs) {
   return lhs._data == rhs._data;
+}
+
+int TimeHourMinute::difftime(const TimeHourMinute& time_end, const TimeHourMinute& time_start) {
+  return (time_end.getHour() - time_start.getHour()) * 3600 +
+         (time_end.getMinute() - time_start.getMinute()) * 60;
 }
--- firmware/src/gpio_module.cpp
+++ firmware/src/gpio_module.cpp
@@ -9,20 +9,19 @@
 
 namespace gpio {
   namespace {
-    constexpr nrfx_gpiote_pin_t PIN_OUT = NRF_GPIO_PIN_MAP(0, 13);
-    constexpr nrfx_gpiote_pin_t PIN_IN  = NRF_GPIO_PIN_MAP(0, 11);
+    // constexpr nrfx_gpiote_pin_t PIN_OUT = NRF_GPIO_PIN_MAP(0, 13);
 
-    void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action) {
-      //   nrf_drv_gpiote_out_clear(PIN_OUT);
-      //   nrf_drv_gpiote_out_set(PIN_OUT);
-      nrf_drv_gpiote_out_set(PIN_OUT);
+    // void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action) {
+    //   //   nrf_drv_gpiote_out_clear(PIN_OUT);
+    //   //   nrf_drv_gpiote_out_set(PIN_OUT);
+    //   nrf_drv_gpiote_out_set(PIN_OUT);
 
-      auto& rtc = RV3028::get();
-      rtc.disableAlarmInterrupt();
-      rtc.clearAlarmInterruptFlag();
-      rtc.disableTimerInterrupt();
-      rtc.clearTimerInterruptFlag();
-    }
+    //   auto& rtc = RV3028::get();
+    //   rtc.disableAlarmInterrupt();
+    //   rtc.clearAlarmInterruptFlag();
+    //   rtc.disableTimerInterrupt();
+    //   rtc.clearTimerInterruptFlag();
+    // }
   }  // namespace
   void init() {
     ret_code_t err_code;
@@ -30,19 +29,19 @@ namespace gpio {
     err_code = nrf_drv_gpiote_init();
     APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_out_config_t out_config = GPIOTE_CONFIG_OUT_SIMPLE(false);
+    // nrf_drv_gpiote_out_config_t out_config = GPIOTE_CONFIG_OUT_SIMPLE(false);
 
-    err_code = nrf_drv_gpiote_out_init(PIN_OUT, &out_config);
-    APP_ERROR_CHECK(err_code);
+    // err_code = nrf_drv_gpiote_out_init(PIN_OUT, &out_config);
+    // APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_out_clear(PIN_OUT);
+    // nrf_drv_gpiote_out_clear(PIN_OUT);
 
-    nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
-    in_config.pull                       = NRF_GPIO_PIN_PULLUP;
+    // nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
+    // in_config.pull                       = NRF_GPIO_PIN_PULLUP;
 
-    err_code = nrf_drv_gpiote_in_init(PIN_IN, &in_config, in_pin_handler);
-    APP_ERROR_CHECK(err_code);
+    // err_code = nrf_drv_gpiote_in_init(PIN_IN, &in_config, in_pin_handler);
+    // APP_ERROR_CHECK(err_code);
 
-    nrf_drv_gpiote_in_event_enable(PIN_IN, true);
+    // nrf_drv_gpiote_in_event_enable(PIN_IN, true);
   }
 }  // namespace gpio
